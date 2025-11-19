cae59354a580b925cb4b9f514cc9c4d786f28e46
Yevhenii Pozniak <jekamanu@gmail.com>
2025-11-19 17:52:54 +0200

    update

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import { getEnvVar } from '../utils/getEnvVar.js';
+
+export async function initMongoConnection() {
+  try {
+    const user = getEnvVar('MONGODB_USER');
+    const pwd = getEnvVar('MONGODB_PASSWORD');
+    const url = getEnvVar('MONGODB_URL');
+    const db = getEnvVar('MONGODB_DB');
+
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?appName=Cluster0`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
+    console.log('Error while setting up mongo connection', error);
+    throw error;
+  }
+}
